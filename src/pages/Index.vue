ab2f3a40efcf71a777c056d328f39c545daf0fe7
Eduardo Braga <eduardocbraga@hotmail.com>
2022-10-01 20:52:07 -0300

    feat: consome dados da planilha google escolhida

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -2,13 +2,13 @@
   <Layout>
 
     <!-- Learn how to use images here: https://gridsome.org/docs/images -->
-    <g-image alt="Example image" src="~/favicon.png" width="135" />
 
     <h1>Hello, world!</h1>
-
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur excepturi labore tempore expedita, et iste tenetur suscipit explicabo! Dolores, aperiam non officia eos quod asperiores
-    </p>
+    <div v-for="edge in $page.posts.edges" :key="edge.node.id">
+      <p>{{ edge.node.name }}</p>
+      <p>{{ edge.node.age }}</p>
+      <p>{{ edge.node.date }}</p>
+    </div>
 
     <p class="home-links">
       <a href="https://gridsome.org/docs/" target="_blank" rel="noopener">Gridsome Docs</a>
@@ -17,6 +17,20 @@
 
   </Layout>
 </template>
+<page-query>
+  query {
+   posts: allGoogleSheet {
+    edges {
+      node {
+        id
+        name
+        age
+        date
+      }
+    }
+  }
+  }
+</page-query>
 
 <script>
 export default {
